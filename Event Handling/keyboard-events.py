192ac9d76c90a286a06004d05bf2e0e5b5a2d3e8
Rafal Jakobsen <rafal.jakobsen@protonmail.com>
2018-09-26 23:04:30 +0200

    Event Handling 1. Configuring command callbacks 2. Binding to keyboard events 3. Binding to mouse events 4. Binding to virtual events 5. Binding to multiple events 6. Reformatting code

---
+++ Event Handling/keyboard-events.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+
+def key_press(event):
+    print("type: {} ".format(event.type))
+    print("widget: {} ".format(event.widget))
+    print("char: {} ".format(event.char))
+    print("keysym: {} ".format(event.keysym))
+    print("keycode: {} ".format(event.keycode))
+
+
+def shortcut(action):
+    print(action)
+
+
+root.bind("<Control-c>", lambda e: shortcut("Copy"))
+root.bind("<Control-v>", lambda e: shortcut("Past"))
+
+root.mainloop()
---
+++ Event Handling/mouse-events.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+
+def mouse_press(event):
+    global prev
+    prev = event
+    print("type: {}".format(event.type))
+    print("widget: {}".format(event.widget))
+    print("num: {}".format(event.num))
+    print("x: {}".format(event.x))
+    print("y: {}".format(event.y))
+    print("x root: {}".format(event.x_root))
+    print("y root: {}".format(event.y_root))
+
+
+def draw(event):
+    global prev
+    canvas.create_line(prev.x, prev.y, event.x, event.y, width=5)
+    prev = event
+
+
+canvas = Canvas(root, width=640, height=480, background="white")
+canvas.pack()
+
+canvas.bind("<ButtonPress>", mouse_press)
+canvas.bind("<B1-Motion>", draw)
+
+root.mainloop()
